59f35e8363a7c5082dde96ba5ba0d8834926482d
Sonu Kuruvila <sonukuruvila1998@gmail.com>
2021-10-11 20:46:52 +0530

    Add files via upload

---
+++ Level_3/inSufficientBalanceException.java
@@ -0,0 +1,12 @@
+package Level_3;
+
+public class inSufficientBalanceException extends Exception {
+
+	public inSufficientBalanceException() {
+		
+	}
+	public inSufficientBalanceException(String message) {
+		super(message);
+	}
+
+}
